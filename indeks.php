5776be7c3a31159db3d05c7f31507031c00c980f
Taqizdihar <m.taqizdihar@.com>
2025-04-28 21:02:24 +0700

    magnus

---
+++ indeks.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include "views/header.php";
+?>
+<main>
+<?php
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+        $allowed_pages = ['about','mahasiswa','addmahasiswa','editmahasiswa','login'];
+        
+        if (in_array($page, $allowed_pages)) {
+            include "pages/$page.php";
+        } else {
+            echo "<h2>404 - Halaman tidak ditemukan</h2>";
+        }
+    } else {
+        include "login.php";
+    }
+?>
+</main>
+<?php
+include "Views/sidebar.php";
+include "views/footer.php";  
+?>
