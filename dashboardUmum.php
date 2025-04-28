26dadc899ba5a919a05549035890b7206f52a470
Taqizdihar <m.taqizdihar@.com>
2025-04-28 22:11:27 +0700

    sa

--- dashboardUmum.php
+++ dashboardUmum.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Wanderlust</title>
-    <link rel="stylesheet" href="Photos-CSSumum/dashboardUmum.css">
+    <link rel="stylesheet" href="/Photos-CSSumum/dashboardUmum.css">
 </head>
 <body>
     <div class="logo">
-        <img src="Photos-CSSumum/Wanderings for Wonders.png" alt="Wanderlust Logo">
+        <img src="/Photos-CSSumum/Wanderings for Wonders.png" alt="Wanderlust Logo">
     </div>
     <div class="buttons-container">
         <h3>Wanderings for Wonders</h3>
