5cf89181a02a7a7678b8b2004bc15af64bfacb16
Taqizdihar <m.taqizdihar@.com>
2025-04-29 05:30:48 +0700

    Magnum Opus

--- dashboardUmum.php
+++ dashboardUmum.php
@@ -4,16 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Wanderlust</title>
-    <link rel="stylesheet" href="/Photos-CSSumum/dashboardUmum.css">
+    <link rel="stylesheet" href="Photos-CSSumum/dashboardUmum.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=MuseoModerno|Concert One">
 </head>
 <body>
-    <div class="logo">
-        <img src="/Photos-CSSumum/Wanderings for Wonders.png" alt="Wanderlust Logo">
-    </div>
-    <div class="buttons-container">
-        <h3>Wanderings for Wonders</h3>
-        <div class="buttons">
-
+    <div class="main">
+        <div class="logo">
+            <h2>Wander all around with</h2>
+        </div>
+        <div class="buttons-container">
+            <h3>Join us now</h3>
+            <div class="buttons">
+                <a href="login.php" id="login">Log-In</a>
+                <a href="signin" id="signin">Sign-In</a>
+            </div>
         </div>
     </div>
 </body>
