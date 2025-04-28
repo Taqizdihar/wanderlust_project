c2719cbb1df641f614ee3e5a253299fff0cb8c1e
Taqizdihar <m.taqizdihar@.com>
2025-04-28 13:02:53 +0700

    a

---
+++ pengguna/Tips.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tips & Trick</title>
+    <link rel="stylesheet" href="Wanderlust.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=MuseoModerno|Concert One">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- Navbar/header -->
+    <header>
+        <div class="header-content">
+          <div class="header-logo">
+            <img src="Images/Wanderlust Logo Circle.png" height="50" width="50" alt="Wanderlust Logo">
+            <a href="Home.html">Wanderlust</a>
+          </div>
+          <nav class="navbar">
+            <ul>
+              <li><a href="Login.html">Login</a></li>
+              <li><a href="Promo.html">Promo</a></li>
+              <li><a href="PemesananTiket.html">Tiket</a></li>
+              <li><a href="Tips.html">Tips</a></li>
+              <li><a href="ContactUs.html">Kontak Kami</a></li>
+              <li><a href="Agenda.html">Agenda</a></li>
+              <li><a href="Profil.html"><img src="Images/PP.jpg" alt="Foto Profil"></a></li>
+            </ul>
+          </nav>
+        </div>
+    </header>
+    <!-- Navbar/header -->
+    
+    <!-- Main Content-->
+    <div class="home-content">
+        <div class="TT-heading">
+            <h2>Tips & Tricks</h2>
+            <h3>Oleh <span>Wanderlust</span></h3>
+        </div>
+        <div class="tip-cards-gallery">
+            <div class="tip-cards-container">
+                <div class="tip-card" style="background-image: url(Images/Beach.png);">
+                    <details>
+                        <summary>Tips Berwisata di Pantai</summary>
+                        <h4 class="tip-opening">1. Pilih Pantai yang Tepat</h4>
+                        <p>
+                          Carilah destinasi pantai yang
+                          sesuai dengan keinginan Anda.
+                          Namun, perhatikan pula segala
+                          informasi mengenai pantai tersebut.
+                          Contohnya seperti tingkat suhu,
+                          tingkat kepadatan ketika libur,
+                          dan lain sebagainya. Oleh karena
+                          itu, gunakan <span>Wanderlust</span> untuk
+                          mencari pantai yang tepat.
+                        </p>
+                        <h5>2. Bawa Sunscreen</h5>
+                        <p>
+                          Mungkin memang terdengar klise.
+                          Namun membawa Sunscreen memang
+                          cukup penting terutama di pantai
+                          dengan suhu yang panas! Jangan
+                          lupa untuk membawa Sunscreen
+                          setiap kali ke pantai, ya.
+                        </p>
+                        <h5>3. Bermain Dengan Hati-Hati</h5>
+                        <p>
+                          Berhati-hatilah ketika Bermain
+                          di pesisir pantai terutama pada
+                          gelombang ombak yang cukup tinggi.
+                          Jangan bermain ketika sore hingga
+                          malam hari di pantai! Kemudian
+                          berhati-hati pula pada karang
+                          yang tajam.
+                        </p>
+                        <h5>4. Perhatikan Kondisi Cuaca</h5>
+                        <p>
+                          Selain berhati-hati ketika
+                          bermain di pantai, perhatikan
+                          pula kondisi cuaca sebelum
+                          maupun ketika bermain karena
+                          cuaca yang buruk bisa membawa
+                          bahaya! Teruslah melihat
+                          prakiraan cuaca sebagai bentuk
+                          antisipasi.
+                        </p>
+                    </details>
+                </div>
+            </div>
+            <div class="tip-cards-container">
+                <div class="tip-card" style="background-image: url(Images/Camp.jpg);">
+                    <details>
+                        <summary>Tips Berkemah</summary>
+                        <h4 class="tip-opening">1. Persiapan yang Matang</h4>
+                        <p>
+                          Sebelum berangkat berkemah,
+                          pastikan Anda sudah membawa
+                          segala peralatan dan bahan
+                          yang diperlukan seperti tenda,
+                          sleeping bag, makanan, minuman,
+                          baju hangat, P3K, dan hal esensial
+                          lainnya yang harus selalu
+                          Anda perhatikan.
+                        </p>
+                        <h5>2. Cari Lokasi yang Tepat</h5>
+                        <p>
+                          Berkemah di tempat yang salah
+                          bisa berbahaya bagi kita semua.
+                          Oleh karena itu, pilih lokasi
+                          berkemah yang aman, terpercaya,
+                          dan terawasi oleh pihak berwenang
+                          setempat. <span>Wanderlust</span>
+                          menyediakan berbagai tempat berkemah
+                          aman dan terpercaya untuk Anda.
+                        </p>
+                        <h5>3. Ilmu Berkemah</h5>
+                        <p>
+                          Jangan lupa untuk mempelajari
+                          segala ilmu berkemah atau Ilmu
+                          Pramuka yang bisa diterapkan
+                          dalam kondisi di alam bebas
+                          ketika berkemah. Setiap ilmu
+                          yang dipelajari pasti bisa
+                          menjadi bermanfaat.
+                        </p>
+                        <h5>4. Bersama Teman</h5>
+                        <p>
+                          Ketika akan melakukan kemah,
+                          sangat disarankan untuk selalu
+                          mengajak teman karena segala
+                          sesuatu akan terasa lebih
+                          menantang di alam! Pastikan
+                          teman Anda juga senang
+                          berkemah, ya.
+                        </p>
+                    </details>
+                </div>
+            </div>
+            <div class="tip-cards-container">
+                <div class="tip-card" style="background-image: url(Images/Traffic.jpg);">
+                    <details>
+                        <summary>Tips Macet Liburan</summary>
+                        <h4 class="tip-opening">1. Jauh-Jauh Hari</h4>
+                        <p>
+                          Untuk menghindari kepadatan
+                          jalan ketika liburan, hal
+                          paling sederhana yang bisa
+                          dilakukan sebagai bentuk
+                          antisipasi adalah memesan
+                          tiket wisata dari jauh-jauh
+                          hari maupun berangkat ke
+                          destinasi wisata sebelum hari
+                          perkiraan macet.
+                        </p>
+                        <h5>2. Transportasi Umum</h5>
+                        <p>
+                          Menggunakan transportasi
+                          umum bisa mengurangi kemacetan
+                          pada jalan ketika musim Liburan
+                          tiba. Oleh karena itu, <span>Wanderlust</span>
+                          menyediakan tiket ke berbagai destinasi
+                          wisata yang terpercaya.
+                        </p>
+                    </details>
+                </div> 
+            </div>
+            <div class="tip-cards-container">
+                <div class="tip-card" style="background-image: url(Images/Card.jpg);">
+                    <details>
+                        <summary>Awas Penipuan Wisata!</summary>
+                        <h4 class="tip-opening">1. Tiket Palsu</h4>
+                        <p>
+                          Berhati-hatilah ketika
+                          membeli tiket dari pihak
+                          yang tidak sah karena
+                          kemungkinan besar merupakan
+                          penipuan. Oleh karena itu,
+                          gunakan <span>Wanderlust</span>
+                          untuk mendapatkan tiket yang
+                          asli dan sah.
+                        </p>
+                        <h5>2. Suvenir Palsu</h5>
+                        <p>
+                          Perhatikan setiap suvenir
+                          yang terdapat pada tempat
+                          wisata yang Anda kunjungi.
+                          Terkadang terdapat beberapa
+                          suvenir palsu yang dijual
+                          cukup mahal dan menipu
+                          kebanyakan orang. Jadilah
+                          wisatawan yang cermat.
+                        </p>
+                    </details>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Main Content-->
+
+    <!--Footer-->
+    <footer>
+        <div class="footer-container">
+          <div class="footer-logo">
+            <img src="Images/Wanderlust Logo Circle.png" height="70" width="70" alt="Wanderlust Logo">
+            <div>
+              <h5>
+                Wanderlust
+                <span style="display: block; font: 15px 'Concert One', sans-serif;">Wander for Wonders</span>
+              </h5>
+            </div>
+          </div>
+          <div class="footbar">
+            <table>
+              <tr>
+                <td><a href="AboutUs.html">Tentang Kami</a></td>
+                <td><a href="Komunitas.html">Komunitas</a></td>
+                <td><a href="Profil.html">Profil</a></td>
+              </tr>
+              <tr>
+                <td><a href="ContactUs.html">Kontak Kami</a></td>
+                <td><a href="Tips.html">Tips & Trick</a></td>
+                <td><a href="Agenda.html">Agenda</a></td>
+              </tr>
+              <tr>
+                <td><a href="FAQs.html">FAQs</a></td>
+                <td><a href="Promo.html">Promo</a></td>
+                <td><a href="Home.html">Home</a></td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <p>Copyright © 2024 Wanderlust. All rights reserved</p>
+    </footer>
+    <!--Footer-->
+</body>
+</html>
