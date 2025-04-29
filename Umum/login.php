933f4ebcd8dc631c17e33c25570162dcee0f6f04
Taqizdihar <m.taqizdihar@.com>
2025-04-29 18:38:01 +0700

    a

--- Umum/login.php
+++ Umum/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="Photos-CSSumum/login.css">
+    <link rel="stylesheet" href="cssUmum/login.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=MuseoModerno|Concert One">
 </head>
 <body>
