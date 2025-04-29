1d8b6168ec5f0e0b6226b04e55393d49f674edf3
Taqizdihar <m.taqizdihar@.com>
2025-04-29 18:15:58 +0700

    Magnus

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+    
+$dbname = "wanderlust";
+    
+$conn = mysqli_connect($host, $user, $password, $dbname);
+if (!$conn) {
+    die("Koneksi ke MySQL gagal");
+}
+?>
--- login.php
+++ login.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="Photos-CSSumum/login.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=MuseoModerno|Concert One">
 </head>
 <body>
-    <div class="logo">
-
-    </div>
-    <div class="buttons">
+    <div class="login-container">
         
+    </div>
+    <div class="footer">
+        <img src="Photos-CSSumum/Wanderings for Wonders side.png" alt="Wanderlust Logo">
     </div>
 </body>
 </html>
