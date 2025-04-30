4faf6cb37e90929841b0f302c322f3f7c71c5db4
Taqizdihar <m.taqizdihar@.com>
2025-04-30 13:55:26 +0700

    samas

--- Umum/choice.php
+++ Umum/choice.php
@@ -6,11 +6,29 @@
     <title>Your Role</title>
     <link rel="stylesheet" href="Umum/cssUmum/choice.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=MuseoModerno|Concert One">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <div class="role-container">
         <h2>Choose Your Role</h2>
-
+        <div class="role-buttons">
+            <div class="role">
+                <a href="">
+                    <i class="fa-solid fa-user"></i>
+                    <div class="role-text">
+                        <h4>Test</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="role">
+                <a href="">
+                    <i class="fa-solid fa-user"></i>
+                    <div class="role-text">
+                        <h4>Test</h4>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </body>
 </html>
