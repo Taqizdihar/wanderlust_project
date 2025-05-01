a6b000125b41dc8cc20aba66c1f716bd4471ccaa
Taqizdihar <m.taqizdihar@.com>
2025-05-01 21:59:14 +0700

    Merves

--- pengelolaWisata/dashboardWisata.php
+++ pengelolaWisata/dashboardWisata.php
@@ -1,27 +1,35 @@
+<?php
+include "../config.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
     <link rel="stylesheet" href="pengelolaWisata/cssWisata/dashboardWisata.css?v=1.0.4">
+    <link rel="stylesheet" href="cssWisata/dashboardWisata.css?v=1.0.4">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=MuseoModerno|Concert One">
 </head>
 <body>
 
 <div class="navbar">
+    <img src="" alt="Wanderlust Logo">
     <h1>Partner Dashboard</h1>
-    <a href="logout.php">Logout</a> 
+    <a href="logout.php"><i class="fa-regular fa-circle-user"></i></a> 
 </div>
 
 <div class="sidebar">
-    <a href="#">Dashboard</a>
-    <a href="#">Pengguna</a>
-    <a href="#">Laporan</a>
-    <a href="#">Pengaturan</a>
+    <a href="dashboardWisata.php">Dashboard</a>
+    <a href="../notFound.php">Places</a>
+    <a href="../notFound.php">Orders</a>
+    <a href="../notFound.php">Help Centre</a>
+    <a href="../notFound.php">Log Out</a>
 </div>
 
 <div class="main">
-    <h2>Selamat Datang, Admin!</h2>
+    <h2>Welcome, </h2>
 
     <div class="card">
         <h3>Total Pengguna</h3>
