1f543bc6d6f12a934a6b016393c43ef03c826410
faizsyafiqn <faizsyafiqn@gmail.com>
2025-05-02 13:37:55 +0700

    git push origin main main

--- pengguna/CariDestinasi.php
+++ pengguna/CariDestinasi.php
@@ -1,9 +1,11 @@
+<?php
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cari Kegiatan</title>
+    <title>Cari Destinasi</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=MuseoModerno|Concert One">
     <link rel="stylesheet" href="searchdesti.css">
 </head>
@@ -93,18 +95,7 @@
         </div>
     </div>
     <div class="container">
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Travel Recommendations</title>
-        <link rel="stylesheet" href="styles.css">
-        </head>
-        <body>
-            <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travel Recommendations</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
+        
     <div class="activities">
         <h2>Top Destinasi</h2>
         <div class="card-container">
