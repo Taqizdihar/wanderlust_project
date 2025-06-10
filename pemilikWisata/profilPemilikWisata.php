433f954915f87eda7c32eeaa5a887f1d78f31409
Taqizdihar <m.taqizdihar@.com>
2025-06-10 13:19:39 +0700

    a

--- pemilikWisata/profilPemilikWisata.php
+++ pemilikWisata/profilPemilikWisata.php
@@ -19,12 +19,12 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Profile</title>
-    <link rel="stylesheet" href="pengelolaWisata/cssWisata/profilPemilikWisata.css">
+    <link rel="stylesheet" href="pemilikWisata/cssWisata/profilPemilikWisata.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=MuseoModerno|Concert One">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <?php include "pengelolaWisata/viewsWisata.php";?>
+    <?php include "pemilikWisata/viewsWisata.php";?>
 
     <div class="main">
         <div class="profile-container">
@@ -41,17 +41,17 @@ mysqli_close($conn);
         <h2 class="name"><?= $profile['nama']?></h2>
 
             <?php
-                if ($PWProfile['entity_approval'] == 'review') {
+                if ($PWProfile['status'] == 'review') {
             ?>
-                <div class="status" id="review">Status : <?= $PWProfile['entity_approval']?></div>
+                <div class="status" id="review">Status : <?= $PWProfile['status']?></div>
             <?php
-                } else if ($PWProfile['entity_approval'] == 'approved') {
+                } else if ($PWProfile['status'] == 'approved') {
             ?>
-                <div class="status" id="approved">Status : <?= $PWProfile['entity_approval']?></div>
+                <div class="status" id="approved">Status : <?= $PWProfile['status']?></div>
             <?php
-                } else if ($PWProfile['entity_approval'] == 'rejected') {
+                } else if ($PWProfile['status'] == 'rejected') {
             ?>
-                <div class="status" id="rejected">Status : <?= $PWProfile['entity_approval']?></div>
+                <div class="status" id="rejected">Status : <?= $PWProfile['status']?></div>
             <?php
                 }
             ?>
@@ -62,20 +62,20 @@ mysqli_close($conn);
             <div class="value-box"><?= $profile['email']?></div>
 
             <p>Phone</p>
-            <div class="value-box"><?= $profile['phonenumber']?></div>
+            <div class="value-box"><?= $profile['no_telepon']?></div>
             </div>
 
             <div class="info-right">
             <p>Legal Tax Document</p>
-            <a href="pengelolaWisata/photos/<?= $PWProfile['tax_document']?>" target="_blank" class="doc-btn">See Document <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+            <a href="pengelolaWisata/photos/<?= $PWProfile['npwp']?>" target="_blank" class="doc-btn">See Document <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
 
             <p>Legal Business Document</p>
-            <a href="pengelolaWisata/photos/<?= $PWProfile['legal_business_document']?>" target="_blank" class="doc-btn">See Document <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+            <a href="pengelolaWisata/photos/<?= $PWProfile['siup']?>" target="_blank" class="doc-btn">See Document <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
             </div>
         </div>
         <div class="address">
             <p>Address</p>
-            <div class="value-box"><?= $PWProfile['legal_document_address']?></div>
+            <div class="value-box"><?= $PWProfile['alamat_bisnis']?></div>
         </div>
 
         <a href="indeks.php?page=editProfilWisata" class="edit-btn">Edit Identity</a>
--- pemilikWisata/seeWisata.php
+++ pemilikWisata/seeWisata.php
@@ -2,7 +2,7 @@
 include "config.php";
 
 $ID = $_SESSION['user_id'];
-$lokasi_id = $_GET['id_lokasi'];
+$lokasi_id = $_GET['tempatwisata_id'] ?? null;
 $sqlStatement1 = "SELECT * FROM user WHERE user_id='$ID'";
 $query = mysqli_query($conn, $sqlStatement1);
 $profile = mysqli_fetch_assoc($query);
@@ -11,16 +11,16 @@ $sqlStatement2 = "SELECT * FROM pemilikwisata WHERE pw_id='$ID'";
 $query = mysqli_query($conn, $sqlStatement2);
 $PWProfile = mysqli_fetch_assoc($query);
 
-$sqlStatement3 = "SELECT * FROM lokasi WHERE id_lokasi='$lokasi_id'";
+$sqlStatement3 = "SELECT * FROM tempatwisata WHERE tempatwisata_id='$lokasi_id'";
 $query3 = mysqli_query($conn, $sqlStatement3);
 $dataLokasi = mysqli_fetch_assoc($query3);
 
-$sqlStatement4 = "SELECT url_photo FROM foto_lokasi WHERE id_lokasi='$lokasi_id' ORDER BY urutan";
+$sqlStatement4 = "SELECT link_foto FROM fotowisata WHERE tempatwisata_id='$lokasi_id' ORDER BY urutan";
 $foto = mysqli_query($conn, $sqlStatement4);
 $fotos = [];
 
 while ($barisTabel = mysqli_fetch_assoc($foto)) {
-    $fotos[] = $barisTabel['url_photo'];
+    $fotos[] = $barisTabel['link_foto'];
 }
 ?>
 
@@ -30,7 +30,7 @@ while ($barisTabel = mysqli_fetch_assoc($foto)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detail Tempat Wisata</title>
-    <link rel="stylesheet" href="pengelolaWisata/cssWisata/seeWisata.css">
+    <link rel="stylesheet" href="pemilikWisata/cssWisata/seeWisata.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=MuseoModerno|Concert One">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -42,11 +42,11 @@ while ($barisTabel = mysqli_fetch_assoc($foto)) {
         <div class="container">
             <button class="back-button" onclick="history.back()">← Kembali</button>
             
-            <img src="pengelolaWisata/photos/<?= $fotos[0]?>" alt="Foto Utama Lokasi" class="main-image">
+            <img src="pemilikWisata/foto/<?= $fotos[0]?>" alt="Foto Utama Lokasi" class="main-image">
             
             <div class="image-grid">
                 <?php for ($i=1; $i < count($fotos); $i++) { ?>
-                    <img src="pengelolaWisata/photos/<?= $fotos[$i]?>" alt="Foto 1" class="grid-image">
+                    <img src="pemilikWisata/foto/<?= $fotos[$i]?>" alt="Foto 1" class="grid-image">
                 <?php }?>
             </div>
             
@@ -54,11 +54,6 @@ while ($barisTabel = mysqli_fetch_assoc($foto)) {
                 <h1 class="location-name"><?= $dataLokasi['nama_lokasi'] ?></h1>
                 <p class="location-type"><?= $dataLokasi['jenis_wisata'] ?></p>
                 <p class="location-hours"><?= $dataLokasi['waktu_buka'] ?> - <?= $dataLokasi['waktu_tutup'] ?></p>
-                
-                <div class="ticket-info">
-                    <p class="ticket-item"><strong>Harga Tiket: </strong><?= $dataLokasi['harga_tiket'] ?></p>
-                    <p class="ticket-item"><strong>Kuota Tiket: </strong><?= $dataLokasi['jumlah_tiket'] ?></p>
-                </div>
             </div>
             
             <div class="description">
@@ -70,7 +65,7 @@ while ($barisTabel = mysqli_fetch_assoc($foto)) {
             
             <div class="legal-document">
                 <p><strong>Nomor PIC: </strong><?= $dataLokasi['nomor_pic'] ?></p>
-                <a href="pengelolaWisata/photos/<?= $dataLokasi['surat_izin']?>" target="_blank">View Legal Document</a>
+                <a href="pemilikWisata/photos/<?= $dataLokasi['surat_izin']?>" target="_blank">View Legal Document</a>
             </div>
         </div>
     </div>
