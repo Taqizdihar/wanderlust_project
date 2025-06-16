973adc1c7fac21bf96bd5a109e671dc7aedd8a24
faizsyafiqn <faizsyafiqn@gmail.com>
2025-06-16 08:55:03 +0700

    as

--- indeks.php
+++ indeks.php
@@ -26,38 +26,6 @@ if (isset($_GET['page'])) {
         include "pengguna/$page.php";
     } else {
         echo "<h2>404 - Halaman tidak ditemukan</h2>";
-<<<<<<< HEAD
-=======
-=======
-session_start();
-?>
-<main>
-<?php
-    if (isset($_GET['page'])) {
-        $page = $_GET['page'];
-        $pages_umum = ['login', 'signin', 'homeUmum', 'choice', 'logout'];
-        $pages_pw = ['verifikasiEntitas', 'dashboardWisata', 'profilPemilikWisata', 'editProfilWisata', 'daftarWisata', 'addWisata', 'editWisata', 'deleteWisata', 'seeWisata', 'addPaket', 'daftarPaket', 'kelolaPaket', 'deletePaket'];
-        $pages_admin = ['acc', 'accpengolah', 'accproperti', 'acctransaksi', 'dashboardAdmin', 'pengolahStatus', 'propertiStatus', 'verifikasiTopUp', 'memberlist', 'member', 'accwisata', 'transaksiverifikasi'];
-
-        $pages_wisatawan = ['Home', 'detailDestinasiWisata', 'reservasiTiket', 'Favorit', 'Pencarian', 'Profil', 'editProfil', 'Saldo', 'topUpSaldo', 'riwayatReservasi'];
-        
-        if (in_array($page, $pages_umum)) {
-            include "Umum/$page.php";
-        } else if (in_array($page, $pages_pw)) {
-            include "pemilikWisata/$page.php";
-        } else if (in_array($page, $pages_admin)) {
-
-            include "administrator/$page.php"; 
-        } else if (in_array($page, $pages_wisatawan)) {
-            include "pengguna/$page.php";
-        } else {
-            echo "<h2>404 - Halaman tidak ditemukan</h2>";
-        }
-    } else {
-        // Halaman default jika tidak ada parameter 'page'
-        include "Umum/homeUmum.php";
->>>>>>> 7302301b496f3c305bbe7fc333a582bd99dcc3a5
->>>>>>> d6160a875f9535e3a0248793a9538271db86db6c
     }
 } else {
     include "Umum/homeUmum.php";
