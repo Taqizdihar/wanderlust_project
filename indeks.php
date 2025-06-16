108bd1aa2d6d0b5cc5d05b5948dd17138110865b
Taqizdihar <m.taqizdihar@.com>
2025-06-16 13:00:44 +0700

    as

--- indeks.php
+++ indeks.php
@@ -8,7 +8,7 @@ include "config.php";
     if (isset($_GET['page'])) {
         $page = $_GET['page'];
 
-        $pages_umum = ['login', 'signin', 'homeUmum', 'choice', 'logout'];
+        $pages_umum = ['login', 'signin', 'homeUmum', 'choice', 'logout', 'forgotPassword'];
         $pages_pw = ['verifikasiEntitas', 'dashboardWisata', 'profilPemilikWisata', 'editProfilWisata', 'daftarWisata', 'addWisata', 'editWisata', 'deleteWisata', 'seeWisata', 'addPaket', 'daftarPaket', 'kelolaPaket', 'deletePaket'];
         $pages_admin = ['verifikasi_transaksi', 'transaksiverifikasi', 'acctransaksi', 'dashboardAdmin', 'pengolahStatus', 'propertiStatus', 'verifikasiTopUp', 'memberlist', 'member', 'accwisata', 'acc', 'accpengolah', 'accproperti'];
         $pages_wisatawan = ['Home', 'detailDestinasiWisata', 'reservasiTiket', 'Favorit', 'Pencarian', 'Profil', 'editProfil', 'Saldo', 'topUpSaldo', 'riwayatReservasi'];
