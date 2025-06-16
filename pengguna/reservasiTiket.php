33fab49c3edb44fb42106c147eb4e0ade13782ed
Taqizdihar <m.taqizdihar@.com>
2025-06-16 07:04:46 +0700

    as

--- pengguna/reservasiTiket.php
+++ pengguna/reservasiTiket.php
@@ -64,7 +64,7 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reservasi Tiket Wisata</title>
+    <title>Reserve a ticket</title>
     <style>
         body {
             font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
